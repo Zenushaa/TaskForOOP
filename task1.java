bebd8aef5317be6f818e440a39cb1c9c1e1dda41
Zenusha <nirkajishakya23@gmail.com>
2025-04-23 13:08:43 +0545

    add on

--- task1.java
+++ task1.java
@@ -5,8 +5,61 @@ public class task1 {
         Scanner scanner = new Scanner(System.in);
 
         // Write a Java program to input your full name and print a greeting message.
-        System.out.println("Please enter your full name:");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "! Welcome to the Java programming world.");
+        // System.out.println("Please enter your full name:");
+        // String name = scanner.nextLine();
+        // System.out.println("Hello, " + name + "! Welcome to the Java programming world.");
+
+        // Take two numbers as input and print their sum, difference, product, and quotient.
+        // System.out.println("Please enter two numbers:");
+        // double num1 = scanner.nextDouble();
+        // double num2 = scanner.nextDouble();
+        // System.out.println("Sum: " + (num1 + num2));
+        // System.out.println("Difference: " + (num1 - num2));
+        // System.out.println("Product: " + (num1 * num2));
+        // System.out.println("Quotient: " + (num1 / num2));
+
+        // Write a program to check whether a given number is positive, negative, or zero using if-else.
+        // System.out.println("Please enter a number:");
+        // int number = scanner.nextInt();
+        // if (number > 0) {
+        //     System.out.println("The number is positive.");
+        // } else if (number < 0) {
+        //     System.out.println("The number is negative.");
+        // } else {
+        //     System.out.println("The number is zero.");
+        // }
+
+        // Write a program that takes a number and checks if it is divisible by both 5 and 11.
+        // System.out.println("Please enter a number:");
+        // int num = scanner.nextInt();
+        // if (num % 5 == 0 && num % 11 == 0) {
+        //     System.out.println("The number is divisible by both 5 and 11.");
+        // } else {
+        //     System.out.println("The number is not divisible by both 5 and 11.");
+        // }
+
+        // Input three numbers and print the largest using if-else.
+        // System.out.println("Please enter three numbers:");
+        // int num1 = scanner.nextInt();
+        // int num2 = scanner.nextInt();
+        // int num3 = scanner.nextInt();
+        // if (num1 >= num2 && num1 >= num3) {
+        //     System.out.println("The largest number is: " + num1);
+        // } else if (num2 >= num1 && num2 >= num3) {
+        //     System.out.println("The largest number is: " + num2);
+        // } else {
+        //     System.out.println("The largest number is: " + num3);
+        // }
+
+        // Take a number from the user and print whether it is even or odd.
+        // System.out.println("Please enter a number:");
+        // int number = scanner.nextInt();
+        // if (number % 2 == 0) {
+        //     System.out.println("The number is even.");
+        // } else {
+        //     System.out.println("The number is odd.");
+        // }
+        
+        // Input the age of a person and check if they are eligible to vote (18 or older).
     }
 }
