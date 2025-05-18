07098a24c2e677fce81e4b19ce1da9241e30cb79
Zenusha <nirkajishakya23@gmail.com>
2025-05-18 12:19:26 +0545

    student

---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student{
+    String name;
+    int age;
+    int student_id;
+    void preintDetail(){
+        System.out.println("Name: "+name+"Age: "+age+"Student ID: "+student_id);
+    }
+}
